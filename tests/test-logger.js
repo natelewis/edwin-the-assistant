27d8b70e6d3e258a1fadc7e9f6536015959b074a
Nate Lewis <nlewis@gnetworks.com>
2017-10-15 21:49:29 -0400

    updated logger tests

--- tests/test-logger.js
+++ tests/test-logger.js
@@ -4,21 +4,30 @@ const log = require('../lib/logger');
 
 
 it('should log the correct state to console', () => {
-  // spy on the log.state
   let spy = sinon.spy(console, 'log');
-
   log.state({final: 'done!'});
   assert(spy.calledWith({final: 'done!'}));
-
-  log.statement('hello?');
-  assert(spy.calledWith('    ', [['hello', 'UH'], ['?', '.']]));
-
-  log.info({'yo': 'yo'});
-  assert(spy.calledWith({'yo': 'yo'}));
-
-  log.step({'id': 'step'});
-  assert(spy.calledWith({'id': 'step'}));
-
-  // restore the original function
   spy.restore();
 });
+
+it('should log the correct statement to console', () => {
+  let spy = sinon.spy(console, 'log');
+  log.statement('hello?');
+  assert(spy.calledWith('    ', [['hello', 'UH'], ['?', '.']]));
+  spy.restore();
+});
+
+it('should log the correct info to console', () => {
+  let spy = sinon.spy(console, 'log');
+  log.info({'yo': 'yo'});
+  assert(spy.calledWith({'yo': 'yo'}));
+  spy.restore();
+});
+
+it('should log the correct step to console', () => {
+  let spy = sinon.spy(console, 'log');
+  log.step({'id': 'step'});
+  assert(spy.calledWith({'id': 'step'}));
+  spy.restore();
+});
+
