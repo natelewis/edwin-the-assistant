eaea46f79a59145bf7eb813819074d4a2d64466c
Nate Lewis <nlewis@gnetworks.com>
2017-10-25 18:35:57 -0400

    API will return {} when a topic or intent does not exist yet.

--- data/modules/repeater.js
+++ data/modules/repeater.js
@@ -1,10 +1,30 @@
 module.exports = {run: function(state, config) {
   return new Promise(function(resolve, reject) {
     // given a conversation that I will have a field populated as "payload"
-    // have edwin respond with the said payload.
     state.setFinal(state.getField('payload'));
 
     // I'm done, finish my promise by passing my state back
     return resolve(state);
+
+    /*
+    This could also be done without a module in the topic JSON.  This is here
+    as an example of the simplest module.
+    {
+      "id": "repeater",
+      "annotation": [
+          {
+              "field": "payload",
+              "type": "everythingAfterWord",
+              "startWord": "[intent]"
+          }
+      ],
+      "steps": [
+          {
+              "query": "final"
+              "reply": "[payload]"
+          }
+      ]
+    }
+    */
   });
 }};
